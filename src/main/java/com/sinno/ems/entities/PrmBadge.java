9f76fbc9df5a7041bcbd741f8058312b9118a16a
khalil <khalilrhellab@gmail.com>
2017-03-23 15:20:16 +0000

    K.R ent&mapp

---
+++ src/main/java/com/sinno/ems/entities/PrmBadge.java
@@ -0,0 +1,7 @@
+package com.sinno.ems.entities;
+
+/**
+ * Created by khalil on 23/03/2017.
+ */
+public class PrmBadge {
+}
---
+++ src/main/java/com/sinno/ems/mapper/DriverMapper.java
@@ -0,0 +1,7 @@
+package com.sinno.ems.mapper;
+
+/**
+ * Created by khalil on 22/03/2017.
+ */
+public class DriverMapper {
+}
