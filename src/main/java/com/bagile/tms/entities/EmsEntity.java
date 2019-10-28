368cbb6989ca06fff7eecaab45769c038dca486e
ADIL LAHIB <lahib.adil@gmail.com>
2019-10-28 16:37:04 +0100

    A.L : Added creationDate & updateDate to EmsEntity

--- src/main/java/com/bagile/tms/entities/EmsEntity.java
+++ src/main/java/com/bagile/tms/entities/EmsEntity.java
@@ -2,9 +2,11 @@ package com.bagile.tms.entities;
 
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @MappedSuperclass
 public abstract class EmsEntity implements Serializable {
@@ -14,6 +16,10 @@ public abstract class EmsEntity implements Serializable {
     private static final long serialVersionUID = -4865263538755924621L;
     protected String createdBy;
     protected String updatedBy;
+
+    protected Date creationDate;
+    protected Date updateDate;
+
     protected UsrUser createdByUser;
     protected UsrUser updatedByUser;
 
@@ -37,6 +43,7 @@ public abstract class EmsEntity implements Serializable {
     public void setUpdatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
     }
+
     @JoinColumn(name = "createdbyuserid")
     @ManyToOne(fetch = FetchType.LAZY)
     public UsrUser getCreatedByUser() {
@@ -46,6 +53,7 @@ public abstract class EmsEntity implements Serializable {
     public void setCreatedByUser(UsrUser createdByUser) {
         this.createdByUser = createdByUser;
     }
+
     @JoinColumn(name = "updatedbyuserid")
     @ManyToOne(fetch = FetchType.LAZY)
     public UsrUser getUpdatedByUser() {
@@ -54,5 +62,27 @@ public abstract class EmsEntity implements Serializable {
 
     public void setUpdatedByUser(UsrUser updatedByUser) {
         this.updatedByUser = updatedByUser;
+    }
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JoinColumn(name = "creationdate")
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Temporal(TemporalType.TIMESTAMP)
+    @JoinColumn(name = "updateddate")
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
     }
 }
