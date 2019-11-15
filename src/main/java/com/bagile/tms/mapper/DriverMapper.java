ce421e049d57be815f9549ce47e161e36a5aa009
smekkaoui <smekkaoui@integrateur-si.com>
2019-11-15 16:47:04 +0100

    SM : Modif DriverMapper

--- src/main/java/com/bagile/tms/mapper/DriverMapper.java
+++ src/main/java/com/bagile/tms/mapper/DriverMapper.java
@@ -5,37 +5,33 @@ import com.bagile.tms.entities.TmsDriver;
 
 import java.util.*;
 
-
 public class DriverMapper {
-    private DriverMapper() {
-
+    public DriverMapper() {
     }
 
     private static Map<String, String> map;
 
     static {
         map = new HashMap<>();
-        map.put("id", "tmsDriverId");
-        map.put("code", "tmsDriverCode");
-        map.put("cin", "tmsDriverCin");
-        map.put("birthDate", "tmsDriverbirthDate");
-        map.put("lastMedicalVisit","tmsDriverLastMedicalVisit");
-        map.put("commission", "tmsDriverCommission");
-        map.put("contact", "prmContact");
-        map.put("working", "tmsWorking");
 
+        map.put("id", "tmsDriverId");
+        map.put("registrationNumber", "tmsRegistrationNumber");
+        map.put("code", "tmsDriverCode");
+        map.put("technicalVisit", "tmsDriverTechnicalVisit");
         map.put("creationDate", "creationDate");
         map.put("updateDate", "updateDate");
         map.put("createdBy", "createdBy");
         map.put("updatedBy", "updatedBy");
-
+        map.put("driverCategory", "tmsDriverCategory");
+        map.put("badgeType", "tmsBadgeType");
+        map.put("insurance", "tmsBadgeType");
     }
 
     public static Map<String, String> getMap() {
         return map;
     }
 
-    public String getField(String key) {
+    public static String getField(String key) {
         return map.get(key);
     }
 
@@ -45,34 +41,26 @@ public class DriverMapper {
         }
         TmsDriver tmsDriver = new TmsDriver();
         tmsDriver.setTmsDriverId(driver.getId());
-        tmsDriver.setTmsDriverCode(driver.getCode());
         tmsDriver.setTmsDriverCin(driver.getCin());
-         tmsDriver.setTmsDriverbirthDate(driver.getBirthDate());
-        tmsDriver.setTmsDriverLastMedicalVisit(driver.getLastMedicalVisit());
+        tmsDriver.setTmsDriverCode(driver.getCode() != null ? driver.getCode().toUpperCase() : null);
         tmsDriver.setTmsDriverCommission(driver.getCommission());
-        tmsDriver.setTmsWorking(driver.isWorking());
+        tmsDriver.setTmsDriverLastMedicalVisit(driver.getLastMedicalVisit());
+        tmsDriver.setTmsDriverbirthDate(driver.getBirthDate());
+    
+
 
         tmsDriver.setCreatedBy(driver.getCreatedBy());
         tmsDriver.setUpdatedBy(driver.getUpdatedBy());
         tmsDriver.setCreationDate(driver.getCreationDate());
         tmsDriver.setUpdateDate(driver.getUpdateDate());
 
-        if (!lazy) {
 
-            tmsDriver.setTmsContact(com.bagile.tms.mapper.ContactMapper.toEntity(driver.getContact(),true));
-           // tmsDriver.setTmsDriverCreationUser(UserMapper.toEntity(driver.getCreationUser(),true));
-          //  tmsDriver.setTmsDriverBadges(BadgeMapper.toEntities(driver.getBadges(),true));
-          //  tmsDriver.setTmsDriverVacation(VacationMapper.toEntity(driver.getVacation(),true));
-          //  tmsDriver.setTmsDriverWorkArea(ZoneMapper.toEntity(driver.getWorkArea(),true));
-            //oneToMany(tmsDriver);
+        if (!lazy) {
+            tmsDriver.setTmsContact(ContactMapper.toEntity(driver.getContact(), true));
+          
+
         }
         return tmsDriver;
-    }
-    private static void oneToMany(TmsDriver tmsDriver){
-       // if(null!=tmsDriver.getTmsDriverBadges())
-      //  {
-      //      tmsDriver.getTmsDriverBadges().forEach(tmsBadge -> tmsBadge.setTmsDriver(tmsDriver));
-      //  }
 
     }
 
@@ -81,13 +69,13 @@ public class DriverMapper {
             return null;
         }
         Driver driver = new Driver();
-        driver.setId(tmsDriver.getTmsDriverId());
-        driver.setCode(tmsDriver.getTmsDriverCode());
+        driver.setId((int) tmsDriver.getTmsDriverId());
         driver.setCin(tmsDriver.getTmsDriverCin());
+        driver.setCode(tmsDriver.getTmsDriverCode() != null ? tmsDriver.getTmsDriverCode().toUpperCase() : null);
         driver.setBirthDate(tmsDriver.getTmsDriverbirthDate());
         driver.setLastMedicalVisit(tmsDriver.getTmsDriverLastMedicalVisit());
         driver.setCommission(tmsDriver.getTmsDriverCommission());
-        driver.setWorking(tmsDriver.isTmsWorking());
+
 
         driver.setCreatedBy(tmsDriver.getCreatedBy());
         driver.setUpdatedBy(tmsDriver.getUpdatedBy());
@@ -95,48 +83,46 @@ public class DriverMapper {
         driver.setUpdateDate(tmsDriver.getUpdateDate());
 
         if (!lazy) {
-            driver.setContact(ContactMapper.toDto(tmsDriver.getprmContact(),true));
-           // driver.setCreationUser(UserMapper.toDto(tmsDriver.getTmsDriverCreationUser(),true));
-           // driver.setBadges(BadgeMapper.toDtos(tmsDriver.getTmsDriverBadges(),true));
-          // driver.setVacation(VacationMapper.toDto(tmsDriver.getTmsDriverVacation(),true));
-          //  driver.setWorkArea(ZoneMapper.toDto(tmsDriver.getTmsDriverWorkArea(),true));
+            driver.setContact(ContactMapper.toDto(tmsDriver.getprmContact(), lazy));
+           
+
         }
         return driver;
-    }
 
+    }
 
 
     public static List<Driver> toDtos(Iterable<? extends TmsDriver> tmsDrivers, boolean lazy) {
         if (null == tmsDrivers) {
             return null;
         }
-        List<Driver> drivers = new ArrayList<>();
+        List<Driver> vehicules = new ArrayList<>();
+
+        for (TmsDriver tmsDriver : tmsDrivers) {
+            vehicules.add(toDto(tmsDriver, lazy));
+        }
+        return vehicules;
+    }
+
+    public static Set<TmsDriver> toEntities(Set<Driver> drivers, boolean lazy) {
+        if (null == drivers) {
+            return null;
+        }
+        Set<TmsDriver> tmsDrivers = new HashSet<>();
+        for (Driver driver : drivers) {
+            tmsDrivers.add(toEntity(driver, lazy));
+        }
+        return tmsDrivers;
+    }
+
+    public static Set<Driver> toDtos(Set<TmsDriver> tmsDrivers, boolean lazy) {
+        if (null == tmsDrivers) {
+            return null;
+        }
+        Set<Driver> drivers = new HashSet<>();
         for (TmsDriver tmsDriver : tmsDrivers) {
             drivers.add(toDto(tmsDriver, lazy));
         }
         return drivers;
     }
-    public static Set<TmsDriver> toEntities(Set<? extends Driver>drivers, boolean lazy){
-        if (null == drivers) {
-            return null;
-        }
-        Set<TmsDriver> tmsDrivers = new HashSet<>();
-        for(Driver driver:drivers){
-            tmsDrivers.add(toEntity(driver,lazy));
-        }
-        return tmsDrivers;
-    }
-    public static Set<Driver> toDtos(Set<? extends TmsDriver> tmsDrivers, boolean lazy){
-        if(null == tmsDrivers){
-            return null;
-        }
-        Set<Driver> drivers = new HashSet<>();
-        for(TmsDriver tmsDriver : tmsDrivers){
-            drivers.add(toDto(tmsDriver,lazy));
-        }
-        return drivers;
-    }
-
-
 }
-
