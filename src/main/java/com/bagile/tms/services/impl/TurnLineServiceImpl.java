4e301bf119b42c78323fc5abf673889c810ac26d
ADIL LAHIB <lahib.adil@gmail.com>
2020-04-02 17:32:31 +0100

    A.L: update saleOrderLine from TurnLine

--- src/main/java/com/bagile/tms/services/impl/TurnLineServiceImpl.java
+++ src/main/java/com/bagile/tms/services/impl/TurnLineServiceImpl.java
@@ -1,14 +1,13 @@
 package com.bagile.tms.services.impl;
 
 import com.bagile.tms.dto.TurnLine;
-import com.bagile.tms.dto.TurnLine;
 import com.bagile.tms.entities.TmsTurnLine;
 import com.bagile.tms.exceptions.AttributesNotFound;
-import com.bagile.tms.exceptions.CustomException;
 import com.bagile.tms.exceptions.ErrorType;
 import com.bagile.tms.exceptions.IdNotFound;
 import com.bagile.tms.mapper.TurnLineMapper;
 import com.bagile.tms.repositories.TurnLineRepository;
+import com.bagile.tms.services.SaleOrderLineService;
 import com.bagile.tms.services.TurnLineService;
 import com.bagile.tms.util.Search;
 import org.springframework.data.domain.PageRequest;
@@ -17,6 +16,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,42 +25,54 @@ import java.util.List;
 @Transactional
 public class TurnLineServiceImpl implements TurnLineService {
     private final TurnLineRepository turnLineRepository;
-    
-    public TurnLineServiceImpl(TurnLineRepository turnLineRepository) {
+    private final SaleOrderLineService saleOrderLineService;
+
+    public TurnLineServiceImpl(TurnLineRepository turnLineRepository, SaleOrderLineService saleOrderLineService) {
         this.turnLineRepository = turnLineRepository;
+        this.saleOrderLineService = saleOrderLineService;
     }
 
     @Override
     public TurnLine save(TurnLine turnLine) {
-        return TurnLineMapper.toDto(turnLineRepository.saveAndFlush(TurnLineMapper.toEntity(turnLine, false)), false);
+        TurnLine line = TurnLineMapper.toDto (turnLineRepository.saveAndFlush (TurnLineMapper.toEntity (turnLine, false)), false);
+
+        // TODO
+        if (line.getSaleOrderLine ( ).getQuantityServed ( ) == null) {
+            line.getSaleOrderLine ( ).setQuantityServed (BigDecimal.ZERO);
+        }
+        //line.getSaleOrderLine ( ).setQuantityServed (line.getSaleOrderLine ( ).getQuantityServed ( ).add (line.getQuantityServed ( )));
+        line.getSaleOrderLine ( ).setQantityToLoad (line.getQuantityServed ( ));
+
+        //saleOrderLineService.save (line.getSaleOrderLine ( ));
+
+        return line;
     }
 
     @Override
     public List<TurnLine> saveAll(List<TurnLine> turnLines) {
 
-          List<TurnLine> turnLines1 = new ArrayList<>();
-         for (TurnLine turnLine : turnLines)
-        {
-            turnLines1.add(save(turnLine));
+        List<TurnLine> turnLines1 = new ArrayList<> ( );
+        for (TurnLine turnLine : turnLines) {
+            turnLines1.add (save (turnLine));
         }
 
-         return turnLines1;
+        return turnLines1;
 
     }
 
     @Override
     public Long size() {
-        return turnLineRepository.count();
+        return turnLineRepository.count ( );
     }
 
     @Override
     public Boolean isExist(Long id) {
-        return turnLineRepository.existsById(id);
+        return turnLineRepository.existsById (id);
     }
 
     @Override
     public TurnLine findById(Long id) throws IdNotFound {
-        return TurnLineMapper.toDto(turnLineRepository.findById(id).orElseThrow(() -> new IdNotFound(id)), false);
+        return TurnLineMapper.toDto (turnLineRepository.findById (id).orElseThrow (() -> new IdNotFound (id)), false);
     }
 
     @Override
@@ -70,43 +82,43 @@ public class TurnLineServiceImpl implements TurnLineService {
 
     @Override
     public List<TurnLine> find(String search) throws AttributesNotFound, ErrorType {
-        if (search.equals("")){
-            return findAll ();
+        if (search.equals ("")) {
+            return findAll ( );
         }
-        return TurnLineMapper.toDtos(turnLineRepository.findAll(Search.expression(search, TmsTurnLine.class)), false);
+        return TurnLineMapper.toDtos (turnLineRepository.findAll (Search.expression (search, TmsTurnLine.class)), false);
     }
 
     @Override
     public List<TurnLine> find(String search, int page, int size) throws AttributesNotFound, ErrorType {
-        if (search.equals("")){
+        if (search.equals ("")) {
             return findAll (page, size);
         }
-        Sort sort = Sort.by(Sort.Direction.DESC, "updateDate");
-        Pageable pageable = PageRequest.of(page, size, sort);
-        return TurnLineMapper.toDtos(turnLineRepository.findAll(Search.expression(search, TmsTurnLine.class), pageable), false);
+        Sort sort = Sort.by (Sort.Direction.DESC, "updateDate");
+        Pageable pageable = PageRequest.of (page, size, sort);
+        return TurnLineMapper.toDtos (turnLineRepository.findAll (Search.expression (search, TmsTurnLine.class), pageable), false);
     }
 
     @Override
     public Long size(String search) throws AttributesNotFound, ErrorType {
-        if (search.equals("")){
-            return size ();
+        if (search.equals ("")) {
+            return size ( );
         }
-        return turnLineRepository.count(Search.expression(search, TmsTurnLine.class));
+        return turnLineRepository.count (Search.expression (search, TmsTurnLine.class));
     }
 
     @Override
     public void delete(Long id) {
-        turnLineRepository.deleteById(id);
+        turnLineRepository.deleteById (id);
     }
 
     @Override
     public void delete(TurnLine turnLine) {
-        turnLineRepository.delete(TurnLineMapper.toEntity(turnLine, false));
+        turnLineRepository.delete (TurnLineMapper.toEntity (turnLine, false));
     }
 
     @Override
     public List<TurnLine> findAll() {
-        return TurnLineMapper.toDtos(turnLineRepository.findAll(), false);
+        return TurnLineMapper.toDtos (turnLineRepository.findAll ( ), false);
     }
 
 
@@ -118,9 +130,9 @@ public class TurnLineServiceImpl implements TurnLineService {
 
     @Override
     public List<TurnLine> findAll(int page, int size) {
-        Sort sort = Sort.by(Sort.Direction.DESC, "updateDate");
-        Pageable pageable = PageRequest.of(page, size, sort);
-        return TurnLineMapper.toDtos(turnLineRepository.findAll(pageable), false);
+        Sort sort = Sort.by (Sort.Direction.DESC, "updateDate");
+        Pageable pageable = PageRequest.of (page, size, sort);
+        return TurnLineMapper.toDtos (turnLineRepository.findAll (pageable), false);
     }
 
 }
