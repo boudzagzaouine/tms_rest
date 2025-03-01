da1e8dde0066863927c4c58fe19c81e8b11b2c3b
ismail <ismail@gmail.com>
2025-03-01 18:09:20 +0000

    dfdf

--- src/main/java/com/bagile/gmo/controllers/AccountController.java
+++ src/main/java/com/bagile/gmo/controllers/AccountController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.Arrays;
 import java.util.List;
 
